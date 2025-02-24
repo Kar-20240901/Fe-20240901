2216d30aace599fc63ae184d5596907d45c23a35
Cmc0 <dimlt@vip.qq.com>
2025-02-24 16:15:53 +0800

    feat: 【i】【升级】【代码】

--- src/views/file/fileStorageConfiguration/formEdit.vue
+++ src/views/file/fileStorageConfiguration/formEdit.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref } from "vue";
 import ReCol from "@/components/ReCol";
-import {R} from "@/model/vo/R";
+import { R } from "@/model/vo/R";
 import ReSegmented from "@/components/ReSegmented/src";
-import {doConfirmClick, doOpen} from "@/model/types/IDialogFormProps";
-import {enableFlagOptions} from "@/model/enum/enableFlagEnum";
-import {
-  BaseFileStorageConfigurationInsertOrUpdateDTO
-} from "@/api/http/base/BaseFileStorageConfigurationController";
-import {
-  IFileStorageConfigurationDialogFormProps
-} from "@/views/file/fileStorageConfiguration/types";
-import {yesOrNoOptions} from "@/model/enum/yesOrNoEnum";
-import {BaseFileStorageTypeOptions} from "@/model/enum/BaseFileStorageTypeEnum";
+import { doConfirmClick, doOpen } from "@/model/types/IDialogFormProps";
+import { enableFlagOptions } from "@/model/enum/enableFlagEnum";
+import { BaseFileStorageConfigurationInsertOrUpdateDTO } from "@/api/http/base/BaseFileStorageConfigurationController";
+import { IFileStorageConfigurationDialogFormProps } from "@/views/file/fileStorageConfiguration/types";
+import { yesOrNoOptions } from "@/model/enum/yesOrNoEnum";
+import { BaseFileStorageTypeOptions } from "@/model/enum/BaseFileStorageTypeEnum";
 
 const form = ref<BaseFileStorageConfigurationInsertOrUpdateDTO>({});
 const formRef = ref();
