c44e036792437e1041904585587b7aded6cb9fda
Cmc0 <dimlt@vip.qq.com>
2025-12-05 14:47:47 +0800

    feat: 【i】【优化】【代码】

--- src/views/im/imIndex/contact.vue
+++ src/views/im/imIndex/contact.vue
@@ -97,26 +97,8 @@ function applyOperateClick(value?: string) {
       </template>
     </div>
 
-    <div class="p-2 shrink-0">
-      <div class="bg-neutral-100 rounded-lg px-2 py-1 flex">
-        <template v-for="item in SegmentedOptionArr" :key="item.value">
-          <div
-            :class="
-              'cursor-pointer flex justify-center flex-1 py-2 px-3 transition-all text-sm ' +
-              (menuIndex === item.value
-                ? 'bg-white shadow-sm text-neutral-700 font-medium rounded-md'
-                : 'text-neutral-500 hover:text-neutral-700')
-            "
-            @click="
-              () => {
-                menuSelect(item.value);
-              }
-            "
-          >
-            {{ item.label }}
-          </div>
-        </template>
-      </div>
+    <div class="shrink-0 custom-el-segmented px-2 py-1">
+      <el-segmented v-model="menuIndex" block :options="SegmentedOptionArr" />
     </div>
 
     <div class="flex-1">
@@ -134,3 +116,32 @@ function applyOperateClick(value?: string) {
     <contact-friend-apply-dialog ref="contactFriendApplyDialogRef" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.custom-el-segmented .el-segmented {
+  --el-segmented-item-selected-bg-color: #fff;
+
+  padding: 0.25rem 0.5rem;
+  background-color: #f5f5f5;
+  border-radius: 0.5rem;
+}
+
+:deep(.el-segmented__item) {
+  padding: 0.5rem 0.75rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  color: #737373;
+  border-radius: 0.375rem;
+}
+
+:deep(.el-segmented__item.is-selected) {
+  color: #404040;
+}
+
+:deep(.el-segmented__item-selected) {
+  border-radius: 0.375rem;
+  box-shadow:
+    0 4px 6px -1px rgb(0 0 0 / 10%),
+    0 2px 4px -2px rgb(0 0 0 / 10%);
+}
+</style>
