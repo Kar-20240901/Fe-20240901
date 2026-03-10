57d0ce2fa63a8492a892f3aacbe5674542eeb2dd
Cmc0 <dimlt@vip.qq.com>
2026-03-10 15:35:24 +0800

    feat: 【i】【优化】【代码】

--- src/utils/DateUtil.ts
+++ src/utils/DateUtil.ts
@@ -4,6 +4,13 @@ import dayjs from "dayjs";
  * 格式化时间
  */
 export function FormatDateTime(date: Date = new Date()): string {
+  return dayjs(date).format("YYYY-MM-DD HH:mm:ss");
+}
+
+/**
+ * 格式化时间
+ */
+export function FormatDateTimeFull(date: Date = new Date()): string {
   return dayjs(date).format("YYYY-MM-DD HH:mm:ss.SSS");
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       // 本地跨域代理 https://cn.vitejs.dev/config/server-options.html#server-proxy
       proxy: {
         "/baseApi": {
-          target: "http://192.168.33.11:9080",
+          target: "http://192.168.33.11:80",
           changeOrigin: true,
           rewrite: path => path.replace(/^\/baseApi/, "/prod-api")
         }
